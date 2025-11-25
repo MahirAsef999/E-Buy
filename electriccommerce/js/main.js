147f0eb53e330dbdf6fc1a13e25e8887d3d5f2eb
NemeChris <cchris1@fordham.edu>
2025-11-25 18:09:50 -0500

    partial account

--- electriccommerce/js/main.js
+++ electriccommerce/js/main.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", async () => {
   await updateCartBadge();
   setupProductCards();
+
+  acctBtn = document.getElementById("account-box");
+
+  acctBtn.addEventListener("click", async () => {
+    const response = api("/account");
+    window.location.href = `/${response.userID}`;
+  })
 });
