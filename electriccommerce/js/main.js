fff0e962cd7b19471a33d6d2f71b60aa04284b8f
NemeChris <cchris1@fordham.edu>
2025-12-03 20:35:58 -0500

    updated

--- electriccommerce/js/main.js
+++ electriccommerce/js/main.js
@@ -1,15 +1,6 @@
 document.addEventListener("DOMContentLoaded", async () => {
   await updateCartBadge();
   setupProductCards();
-
-<<<<<<< HEAD
-  acctBtn = document.getElementById("account-box");
-
-  acctBtn.addEventListener("click", async () => {
-    const response = api("/account");
-    window.location.href = `/${response.userID}`;
-  })
-
   document.querySelectorAll(".slider-container").forEach(container =>{
     const slider = container.querySelector(".slider")
     const card = container.querySelector(".card")
@@ -26,13 +17,19 @@ document.addEventListener("DOMContentLoaded", async () => {
       slider.scrollBy({left: -containerWidth, behavior: "smooth"})
     })
   })
+  
+  acctBtn = document.getElementById("account-box");
 
-=======
+  acctBtn.addEventListener("click", async () => {
+    const response = api("/account");
+    window.location.href = `/${response.userID}`;
+  })
+
+
   const acctBtn = document.getElementById("account-box");
   if (acctBtn) {
     acctBtn.addEventListener("click", () => {
       window.location.href = "dashboard.html";
     });
   }
->>>>>>> 84eb68239a3804420ea2c109e29d8ac2baff7793
 });
